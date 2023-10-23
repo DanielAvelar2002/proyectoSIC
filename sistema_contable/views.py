20f108ad22fabf7bd57ae4d09c570c595fddd389
stalinPR2022 <pr17013@ues.edu.sv>
2023-10-22 19:49:38 -0600

    modelos de cuentas y template de catalogo
    Cree los modelos para el manejo de las cuentas y ya esta el template del catalogo

--- sistema_contable/views.py
+++ sistema_contable/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def inicio(request):
@@ -8,8 +9,15 @@ def inicio(request):
 def registroTransaccion(request):
     return render(request, 'transacciones/registro_transaccion.html')    
 
+#Braian
 def catalogoCuentas(request):
-    return render(request, 'transacciones/catalogo_cuentas.html')
+
+    cuentas = Cuenta_Cuenta.objects.all()
+    
+    diccionario_cuentas = {'cuentas': cuentas}
+
+    return render(request, 'transacciones/catalogo_cuentas.html', diccionario_cuentas)
+
 
 def manoObra(request):
     return render(request, 'transacciones/mano_obra.html')    
