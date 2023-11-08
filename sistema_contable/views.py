77e5692aba125bfc3dc2e05ca50a423787a6e8f1
Daniel Avelar <daniel.avelar2002@gmail.com>
2023-11-08 16:19:43 -0600

    Arreglo de las transaacciones
    Funcionan las transacciones, y la Hoja de trabajo obtiene el Balance de Comprobacion y permite realizar ajustes (transacciones). Pero el Balance de Comprobacion se actualiza y eso no debe pasar.

--- sistema_contable/views.py
+++ sistema_contable/views.py
@@ -8,9 +8,9 @@ from django.contrib import messages
 
 # Create your views here.
 def inicio(request):
-    capital = Cuenta.objects.get(codigoCA  = 3101)
-    utilidad = Cuenta.objects.get(codigoCA = 3102)
-    venta = Cuenta.objects.get(codigoCA = 5101)
+    capital = Cuenta.objects.get(codigoCA  = 1) #modificar
+    utilidad = Cuenta.objects.get(codigoCA = 1)
+    venta = Cuenta.objects.get(codigoCA = 1)
     ventasp=venta.saldo 
     total = (capital.saldo+utilidad.saldo) * (-1)
     return render(request, 'transacciones/index.html',{"total":total,"venta":ventasp})
@@ -94,31 +94,48 @@ def balanzaComprobacion(request):
        # Excluye las cuentas de IVA Débito Fiscal y Crédito Fiscal
         if cuenta.nombre not in ["Iva debito fiscal", "Iva credito fiscal"]:
 
-            transacciones_debe = Transaccion.objects.filter(cargo_cuenta=cuenta)
-            transacciones_haber = Transaccion.objects.filter(abono_cuenta=cuenta)
+            transacciones_debe = Transaccion.objects.filter(abono_cuenta=cuenta)
+            transacciones_haber = Transaccion.objects.filter(cargo_cuenta=cuenta)
 
             total_debe = sum(transaccion.monto for transaccion in transacciones_debe)
             total_haber = sum(transaccion.monto for transaccion in transacciones_haber)
 
-            balance.append({
-                'codigo_cuenta': cuenta.codigoCA,  # campo personalizado
-                'nombre_cuenta': cuenta.nombre,
-                'total_debe': total_debe,
-                'total_haber': total_haber,
-            })
+            # Calcula el saldo de la cuenta restando el haber al debe
+            saldo = total_debe - total_haber
+
+            # Determinar en qué columna colocar el saldo
+            if total_debe>total_haber:
+                balance.append({
+                    'codigo_cuenta': cuenta.codigoCA,  # campo personalizado
+                    'nombre_cuenta': cuenta.nombre,
+                    'total_debe': total_debe,
+                    'total_haber': total_haber,
+                    'saldo_debe': saldo,
+                    'saldo_haber': None,
+                })
+            else:
+                balance.append({
+                    'codigo_cuenta': cuenta.codigoCA,  # campo personalizado
+                    'nombre_cuenta': cuenta.nombre,
+                    'total_debe': total_debe,
+                    'total_haber': total_haber,
+                    'saldo_debe': None,
+                    'saldo_haber': saldo,
+                })
 
     total_general_debe = sum(cuenta['total_debe'] for cuenta in balance)
     total_general_haber = sum(cuenta['total_haber'] for cuenta in balance)
 
     return render(request, 'transacciones/balanza_comprobacion.html', {
         'transacciones': transacciones,
-        'balance': balance,
         'total_general_debe': total_general_debe,
         'total_general_haber': total_general_haber,
+        'balance': balance,
     })
 
 
 def hojaTrabajo(request):
+    #logica para el registro de AJUSTES
     if request.method == 'POST':
         form = TransaccionForm(request.POST)
         if form.is_valid():
@@ -131,7 +148,61 @@ def hojaTrabajo(request):
     # Cargar opciones de clases de cuentas y cuentas desde la base de datos
     clases_de_cuentas = ClaseCuenta.objects.all()
     cuentas = Cuenta.objects.all()
-    return render(request, 'transacciones/hoja_trabajo.html',{'form': form, 'clases_de_cuentas': clases_de_cuentas, 'cuentas': cuentas})    
+
+
+    #Logica para traer el balance de comprobacion
+    transacciones = Transaccion.objects.all() #para tener todas las transacciones
+
+    cuentas = Cuenta.objects.all()
+    balance = []
+
+    for cuenta in cuentas:
+       # Excluye las cuentas de IVA Débito Fiscal y Crédito Fiscal
+        if cuenta.nombre not in ["Iva debito fiscal", "Iva credito fiscal"]:
+
+            transacciones_debe = Transaccion.objects.filter(abono_cuenta=cuenta)
+            transacciones_haber = Transaccion.objects.filter(cargo_cuenta=cuenta)
+
+            total_debe = sum(transaccion.monto for transaccion in transacciones_debe)
+            total_haber = sum(transaccion.monto for transaccion in transacciones_haber)
+
+            # Calcula el saldo de la cuenta restando el haber al debe
+            saldo = total_debe - total_haber
+
+            # Determinar en qué columna colocar el saldo
+            if total_debe>total_haber:
+                balance.append({
+                    'codigo_cuenta': cuenta.codigoCA,  # campo personalizado
+                    'nombre_cuenta': cuenta.nombre,
+                    'total_debe': total_debe,
+                    'total_haber': total_haber,
+                    'saldo_debe': saldo,
+                    'saldo_haber': None,
+                })
+            else:
+                balance.append({
+                    'codigo_cuenta': cuenta.codigoCA,  # campo personalizado
+                    'nombre_cuenta': cuenta.nombre,
+                    'total_debe': total_debe,
+                    'total_haber': total_haber,
+                    'saldo_debe': None,
+                    'saldo_haber': saldo,
+                })
+
+    total_general_debe = sum(cuenta['total_debe'] for cuenta in balance)
+    total_general_haber = sum(cuenta['total_haber'] for cuenta in balance)
+
+    return render(request, 'transacciones/hoja_trabajo.html', {
+        #salidas del registro de transacciones
+        'form': form, 
+        'clases_de_cuentas': clases_de_cuentas, 
+        'cuentas': cuentas,
+        #Salidas del balance de comprobacion
+        'transacciones': transacciones,
+        'balance': balance,
+        'total_general_debe': total_general_debe,
+        'total_general_haber': total_general_haber,
+    })
 
 def cierraContable(request):
     return render(request, 'transacciones/cierre_contable.html')    
